a38ffdc974e3b5e346fcbb7f7d1f80fe80b7ddb7
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-07-08 10:36:36 +0100

    Add aboutme view

--- eephotos/portfolio/urls.py
+++ eephotos/portfolio/urls.py
@@ -3,4 +3,5 @@ from portfolio import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
+    url(r'^about/$', views.AboutPageView.as_view()), 
 ]
--- eephotos/portfolio/views.py
+++ eephotos/portfolio/views.py
@@ -3,5 +3,10 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class HomePageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+    template_name = "index.html"
+    # def get(self, request, **kwargs):
+    #     return render(request, 'index.html', context=None)
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
